06f62ed28c4685ffe78c9e3e2aed9d4656da401e
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-06-01 18:30:05 -0700

    Fix font weight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,7 +41,7 @@ export default function App(props: AppProps) {
             },
 
             '.title': {
-              fontWeight: '700'
+              fontWeight: 'bold'
             },
 
             a: {
