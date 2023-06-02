459b3615de4c323358f7c604372d8db9a651cdaa
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-06-01 18:27:59 -0700

    Fix props

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,32 +1,32 @@
 import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
 import Link from 'next/link'
 
-export function ProjectCard({image, title, description, link, tags}) {
+export function ProjectCard(props : any) {
   return (
     
     <div style={{ width: 320, margin: 'auto' }}>
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
         <Image
-          src={image}
+          src={props.image}
           height={160}
         />
       </Card.Section>
 
       <Group position="apart" mt="xs" mb="xs">
-        <Text className='title'>{title}</Text><br/>
+        <Text className='title'>{props.title}</Text><br/>
         <Badge 
         // color="pink" 
         variant="light">
-         {tags}
+         {props.tags}
         </Badge>
       </Group>
 
       <Text size="sm" color="dimmed">
-        {description}
+        {props.description}
       </Text>
 
-      <Link href={link} target='_blank'><Button variant="light" color="blue" fullWidth mt="md" radius="md">
+      <Link href={props.link} target='_blank'><Button variant="light" color="blue" fullWidth mt="md" radius="md">
         See project
       </Button></Link>
     </Card>
